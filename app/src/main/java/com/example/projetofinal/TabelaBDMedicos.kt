da845ee2d42e372788b47adfb35455a45c6dc225
diogoneto <dvnetoubzgmail.com>
2022-06-05 19:39:30 +0100

    Criada classe para representar a tabela de categorias (com a função cria para criar a tabela)

---
+++ app/src/main/java/com/example/projetofinal/TabelaBDMedicos.kt
@@ -0,0 +1,23 @@
+package com.example.projetofinal
+
+import android.database.sqlite.SQLiteDatabase
+import android.provider.BaseColumns
+
+class TabelaBDMedicos(val db: SQLiteDatabase) {
+
+    fun cria() {
+        db.execSQL("CREATE TABLE $NOME (${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT, $CAMPO_NOME TEXT NOT NULL, $CAMPO_ESPECIALIDADE TEXT NOT NULL, $CAMPO_TELEMOVEL INTEGER NOT NULL, $CAMPO_EMAIL TEXT NOT NULL, $CAMPO_SEXO TEXT NOT NULL, $CAMPO_CARTAO_CIDADAO INTEGER NOT NULL)")
+    }
+
+    companion object{
+        const val NOME = "medicos"
+        const val CAMPO_NOME = "nome"
+        const val CAMPO_ESPECIALIDADE = "especialidade"
+        const val CAMPO_TELEMOVEL = "telemovel"
+        const val CAMPO_EMAIL = "email"
+        const val CAMPO_SEXO = "sexo"
+        const val CAMPO_CARTAO_CIDADAO = "cartaocidadao"
+    }
+}
+
+
