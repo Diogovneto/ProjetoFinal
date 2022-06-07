8e6a91582c1061ecaf4bd9e4faf7b37d8fa42e66
diogoneto <dvnetoubzgmail.com>
2022-06-07 22:22:23 +0100

    Criado teste para verificar se é possível alterar parâmetros do médico

--- app/src/androidTest/java/com/example/projetofinal/TestarBaseDados.kt
+++ app/src/androidTest/java/com/example/projetofinal/TestarBaseDados.kt
@@ -1,6 +1,7 @@
 package com.example.projetofinal
 
 import android.database.sqlite.SQLiteDatabase
+import android.provider.BaseColumns
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import org.junit.Assert
@@ -58,83 +59,110 @@ class BaseDadosTest {
     fun consegueInserirMedico() {
         val db = getWritableDatabase()
 
-        insereMedico(db, Medico(
-            "Teste 1",
-        "Consulta",
-        934563467,
-        "teste@gmail.com",
-        "Masculino",
-        14537834))
-
-        insereMedico(db, Medico(
-            "Teste 2",
-            "Consulta",
-            928754328,
-            "teste2@gmail.com",
-            "Feminino",
-            13764567))
-
-        db.close()
-    }
-
-    @Test
-    fun consegueInserirPaciente() {
-        val db = getWritableDatabase()
-
-        inserePaciente(db, Paciente(
-            "Diogo Neto",
-            "27/07/2001",
-            "Masculino",
-            "Rua Teste n285",
-            "3750-598",
-            938059434,
-            "dvnetoubz@gmail.com",
-            14537834,
-            238316050))
-
-        db.close()
-    }
-
-    @Test
-    fun consegueInserirConsulta() {
-        val db = getWritableDatabase()
-
-        val medico = Medico(
+        insereMedico(
+            db, Medico(
                 "Teste 1",
-            "Consulta",
-            928754328,
-            "teste@gmail.com",
-            "Masculino",
-            14537834)
-
-        insereMedico(db, medico)
-
-        val paciente = Paciente(
-            "Diogo Neto123",
-            "27/07/2001",
-            "Masculino",
-            "Rua Teste n285",
-            "3750-598",
-            938059434,
-            "dvnetoubz@gmail.com",
-            14537834,
-            238316050)
-
-        inserePaciente(db, paciente)
-
-
-        val consulta = Consulta(
-            "07/06/2022",
-            "Infantil",
-            "Amarela",
-            "Garganta Inflamada",
-            15,
-            medico.id,
-            paciente.id)
-
-        insereConsulta(db, consulta)
-
-
-        db.close()
+                "Consulta",
+                934563467,
+                "teste@gmail.com",
+                "Masculino",
+                14537834
+            )
+        )
     }
+
+        @Test
+        fun consegueInserirPaciente() {
+            val db = getWritableDatabase()
+
+            inserePaciente(
+                db, Paciente(
+                    "Diogo Neto",
+                    "27/07/2001",
+                    "Masculino",
+                    "Rua Teste n285",
+                    "3750-598",
+                    938059434,
+                    "dvnetoubz@gmail.com",
+                    14537834,
+                    238316050
+                )
+            )
+
+            db.close()
+        }
+
+        @Test
+        fun consegueInserirConsulta() {
+            val db = getWritableDatabase()
+
+            val medico = Medico(
+                "Teste 1",
+                "Consulta",
+                928754328,
+                "teste@gmail.com",
+                "Masculino",
+                14537834
+            )
+
+            insereMedico(db, medico)
+
+            val paciente = Paciente(
+                "Diogo Neto123",
+                "27/07/2001",
+                "Masculino",
+                "Rua Teste n285",
+                "3750-598",
+                938059434,
+                "dvnetoubz@gmail.com",
+                14537834,
+                238316050
+            )
+
+            inserePaciente(db, paciente)
+
+
+            val consulta = Consulta(
+                "07/06/2022",
+                "Infantil",
+                "Amarela",
+                "Garganta Inflamada",
+                15,
+                medico.id,
+                paciente.id
+            )
+
+            insereConsulta(db, consulta)
+
+
+            db.close()
+        }
+
+        @Test
+        fun consegueAlterarMedico() {
+            val db = getWritableDatabase()
+
+            val medico = Medico(
+                "Teste",
+                "Teste",
+                999999999,
+                "asd@gmail.com",
+                "Feminino",
+                11111111
+            )
+            insereMedico(db, medico)
+
+            medico.nome = "Raul Pereira"
+            medico.especialidade = "Cirurgião"
+
+            val registosAlterados = TabelaBDMedicos(db).update(
+                medico.toContentValues(),
+                "${BaseColumns._ID}=?",
+                arrayOf("${medico.id}")
+            )
+
+            Assert.assertEquals(1, registosAlterados)
+
+            db.close()
+        }
 }
