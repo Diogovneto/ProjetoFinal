0a8388518b3acc0d7c8cc125f466802df1462df3
diogoneto <dvnetoubzgmail.com>
2022-06-10 21:48:44 +0100

    Criada função para converter um Cursor num Médico

--- app/src/main/java/com/example/projetofinal/Medico.kt
+++ app/src/main/java/com/example/projetofinal/Medico.kt
@@ -1,6 +1,8 @@
 package com.example.projetofinal
 
 import android.content.ContentValues
+import android.database.Cursor
+import android.provider.BaseColumns
 
 data class Medico(
     var nome: String,
@@ -10,6 +12,7 @@ data class Medico(
     var sexo: String,
     var cartao_cidadao: Long,
     var id: Long = -1) {
+
     fun toContentValues() : ContentValues {
         val valores = ContentValues()
 
@@ -22,4 +25,27 @@ data class Medico(
 
         return valores
     }
+
+    companion object {
+        fun fromCursor(cursor: Cursor): Medico {
+            val posId = cursor.getColumnIndex(BaseColumns._ID)
+            val posNome = cursor.getColumnIndex(TabelaBDMedicos.CAMPO_NOME)
+            val posEspecialidade = cursor.getColumnIndex(TabelaBDMedicos.CAMPO_ESPECIALIDADE)
+            val posTelemovel = cursor.getColumnIndex(TabelaBDMedicos.CAMPO_TELEMOVEL)
+            val posEmail = cursor.getColumnIndex(TabelaBDMedicos.CAMPO_EMAIL)
+            val posSexo = cursor.getColumnIndex(TabelaBDMedicos.CAMPO_SEXO)
+            val posCartaoCidadao = cursor.getColumnIndex(TabelaBDMedicos.CAMPO_CARTAO_CIDADAO)
+
+            val id = cursor.getLong(posId)
+            val nome = cursor.getString(posNome)
+            val especialidade = cursor.getString(posEspecialidade)
+            val telemovel = cursor.getLong(posTelemovel)
+            val email = cursor.getString(posEmail)
+            val sexo = cursor.getString(posSexo)
+            val cartao_cidadao = cursor.getLong(posCartaoCidadao)
+
+            return Medico(nome, especialidade, telemovel, email, sexo, cartao_cidadao, id)
+        }
+    }
+
 }
