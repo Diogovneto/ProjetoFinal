db4d2ed465e0f70646a1f86ab92e7cd88db4b296
diogoneto <dvnetoubzgmail.com>
2022-06-12 16:41:23 +0100

    Criada função para converter um Cursor numa Consulta

--- app/src/main/java/com/example/projetofinal/Consulta.kt
+++ app/src/main/java/com/example/projetofinal/Consulta.kt
@@ -1,6 +1,8 @@
 package com.example.projetofinal
 
 import android.content.ContentValues
+import android.database.Cursor
+import android.provider.BaseColumns
 
 
 class Consulta(
@@ -9,8 +11,9 @@ class Consulta(
     var pulseira_paciente: String,
     var descricao: String,
     var preco: Long,
-    var id: Long = -1,
-    var id2: Long = -1) {
+    var id_medico: Long = -1,
+    var id_paciente: Long = -1,
+    var id: Long = -1) {
 
     fun toContentValues() : ContentValues {
         val valores = ContentValues()
@@ -20,11 +23,36 @@ class Consulta(
         valores.put(TabelaBDConsultas.CAMPO_PULSEIRA_PACIENTE, pulseira_paciente)
         valores.put(TabelaBDConsultas.CAMPO_DESCRICAO, descricao)
         valores.put(TabelaBDConsultas.CAMPO_PRECO, preco)
-        valores.put(TabelaBDConsultas.CAMPO_MEDICO_ID, id)
-        valores.put(TabelaBDConsultas.CAMPO_PACIENTE_ID, id2)
+        valores.put(TabelaBDConsultas.CAMPO_MEDICO_ID, id_medico)
+        valores.put(TabelaBDConsultas.CAMPO_PACIENTE_ID, id_paciente)
 
 
         return valores
     }
+
+    companion object {
+        fun fromCursor(cursor: Cursor): Consulta {
+            val posId = cursor.getColumnIndex(BaseColumns._ID)
+            val posData = cursor.getColumnIndex(TabelaBDConsultas.CAMPO_DATA)
+            val posTipo = cursor.getColumnIndex(TabelaBDConsultas.CAMPO_TIPO)
+            val posPulseiraPaciente = cursor.getColumnIndex(TabelaBDConsultas.CAMPO_PULSEIRA_PACIENTE)
+            val posDescricao = cursor.getColumnIndex(TabelaBDConsultas.CAMPO_DESCRICAO)
+            val posPreco = cursor.getColumnIndex(TabelaBDConsultas.CAMPO_PRECO)
+            val posIdMedico = cursor.getColumnIndex(TabelaBDConsultas.CAMPO_MEDICO_ID)
+            val posIdPaciente = cursor.getColumnIndex(TabelaBDConsultas.CAMPO_PACIENTE_ID)
+
+            val id = cursor.getLong(posId)
+            val data = cursor.getString(posData)
+            val tipo = cursor.getString(posTipo)
+            val pulseira_paciente = cursor.getString(posPulseiraPaciente)
+            val descricao = cursor.getString(posDescricao)
+            val preco = cursor.getLong(posPreco)
+            val id_medico = cursor.getLong(posIdMedico)
+            val id_paciente = cursor.getLong(posIdPaciente)
+
+            return Consulta(data, tipo, pulseira_paciente, descricao, preco, id_medico, id_paciente, id)
+        }
+    }
+
 }
 
