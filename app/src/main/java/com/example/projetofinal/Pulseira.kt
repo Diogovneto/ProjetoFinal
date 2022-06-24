71a3f9f68d6e64dbf5864862fd5f36322a62b9f6
diogoneto <dvnetoubzgmail.com>
2022-06-24 03:16:37 +0100

    Criada função para converter um Cursor numa Pulseira

--- app/src/main/java/com/example/projetofinal/Pulseira.kt
+++ app/src/main/java/com/example/projetofinal/Pulseira.kt
@@ -1,6 +1,8 @@
 package com.example.projetofinal
 
 import android.content.ContentValues
+import android.database.Cursor
+import android.provider.BaseColumns
 
 class Pulseira(var pulseira: String, var id: Long = -1) {
     fun toContentValues() : ContentValues {
@@ -10,4 +12,17 @@ class Pulseira(var pulseira: String, var id: Long = -1) {
 
         return valores
     }
+
+    companion object {
+        fun fromCursor(cursor: Cursor): Pulseira {
+            val posId = cursor.getColumnIndex(BaseColumns._ID)
+            val posPulseira = cursor.getColumnIndex(TabelaBDPulseiras.CAMPO_PULSEIRA)
+
+            val id = cursor.getLong(posId)
+            val pulseira = cursor.getString(posPulseira)
+
+            return Pulseira(pulseira, id)
+        }
+    }
+
 }
