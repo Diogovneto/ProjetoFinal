1a927bc8ca6875b0b46b651e2ef9284afb96f05a
diogoneto <dvnetoubzgmail.com>
2022-06-24 01:03:15 +0100

    Criada função para converter uma Pulseira em ContentValues

--- app/src/main/java/com/example/projetofinal/Pulseira.kt
+++ app/src/main/java/com/example/projetofinal/Pulseira.kt
@@ -1,6 +1,14 @@
 package com.example.projetofinal
 
+import android.content.ContentValues
+
 class Pulseira(var id: Long,
                var pulseira: String) {
+    fun toContentValues() : ContentValues {
+        val valores = ContentValues()
 
+        valores.put(TabelaBDPulseiras.CAMPO_PULSEIRA, pulseira)
+
+        return valores
+    }
 }
