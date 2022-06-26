3ed1d44ad78e60983a32933eac11e71d79dda4fc
diogoneto <dvnetoubzgmail.com>
2022-06-26 21:27:39 +0100

    Criado Adptador de Médicos para comunicar com o RecyclerView

---
+++ app/src/main/java/com/example/projetofinal/AdapterMedicos.kt
@@ -0,0 +1,26 @@
+package com.example.projetofinal
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class AdapterMedicos : RecyclerView.Adapter<AdapterMedicos.ViewHolderMedicos>() {
+    class ViewHolderMedicos(itemMedico: View) : RecyclerView.ViewHolder(itemMedico){
+
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderMedicos {
+        TODO("Not yet implemented")
+    }
+
+
+    override fun onBindViewHolder(holder: ViewHolderMedicos, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
