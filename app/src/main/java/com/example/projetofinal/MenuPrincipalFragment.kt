0505a59a30e2e47cffc2faf07535906787af61ac
diogoneto <dvnetoubzgmail.com>
2022-06-26 19:45:45 +0100

    Desenhada interface do menu principal

--- app/src/main/java/com/example/projetofinal/MenuPrincipalFragment.kt
+++ app/src/main/java/com/example/projetofinal/MenuPrincipalFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import com.example.projetofinal.databinding.FragmentMenuPrincipalBinding
 
 /**
@@ -32,9 +31,9 @@ class MenuPrincipalFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
+        //binding.buttonFirst.setOnClickListener {
+        //    findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        //}
     }
 
     override fun onDestroyView() {
