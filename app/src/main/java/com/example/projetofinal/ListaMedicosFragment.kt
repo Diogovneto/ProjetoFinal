c8f51b6181a40997f0c2cd7cbea4e55f3ecf594d
diogoneto <dvnetoubzgmail.com>
2022-06-26 20:22:46 +0100

    Alterado o nome do SecondFragment para ListaMedicosFragment

--- app/src/main/java/com/example/projetofinal/ListaMedicosFragment.kt
+++ app/src/main/java/com/example/projetofinal/ListaMedicosFragment.kt
@@ -1,19 +1,19 @@
 package com.example.projetofinal
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.example.projetofinal.databinding.FragmentSecondBinding
+import com.example.projetofinal.databinding.FragmentMedicosLabelBinding
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class SecondFragment : Fragment() {
+class ListaMedicosFragment : Fragment() {
 
-    private var _binding: FragmentSecondBinding? = null
+    private var _binding: FragmentMedicosLabelBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,7 +24,7 @@ class SecondFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        _binding = FragmentMedicosLabelBinding.inflate(inflater, container, false)
         return binding.root
 
     }
@@ -33,7 +33,7 @@ class SecondFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+            findNavController().navigate(R.id.action_ListaMedicosFragment_to_MenuPrincipalFragment)
         }
     }
 
