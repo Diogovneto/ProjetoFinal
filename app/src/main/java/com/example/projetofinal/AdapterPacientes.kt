5aea2c806ad89f0c40ac3b8143e691cdd1efaafe
diogoneto <dvnetoubzgmail.com>
2022-07-05 20:23:02 +0100

    Criado Adptador de pacientes para comunicar com o RecyclerView

---
+++ app/src/main/java/com/example/projetofinal/AdapterPacientes.kt
@@ -0,0 +1,24 @@
+package com.example.projetofinal
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class AdapterPacientes: RecyclerView.Adapter<AdapterPacientes.ViewHolderPacientes>() {
+    class ViewHolderPacientes(itemPaciente: View) : RecyclerView.ViewHolder(itemPaciente) {
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderPacientes {
+        TODO("Not yet implemented")
+    }
+
+
+    override fun onBindViewHolder(holder: ViewHolderPacientes, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
