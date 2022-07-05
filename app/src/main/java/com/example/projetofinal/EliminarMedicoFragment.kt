ac2fb158210b908d2da5699cb7c85df44c154d7d
diogoneto <dvnetoubzgmail.com>
2022-07-05 05:35:13 +0100

    Mudar o menu quando passamos para o fragmento eliminar médico

--- app/src/main/java/com/example/projetofinal/EliminarMedicoFragment.kt
+++ app/src/main/java/com/example/projetofinal/EliminarMedicoFragment.kt
@@ -2,43 +2,47 @@ package com.example.projetofinal
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.example.projetofinal.databinding.FragmentEliminarMedicoBinding
 
 class EliminarMedicoFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding: FragmentEliminarMedicoBinding? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_eliminar_medico, container, false)
+        _binding = FragmentEliminarMedicoBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            EliminarMedicoFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        override fun onDestroyView() {
+            super.onDestroyView()
+            _binding = null
+        }
+
+        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+            super.onViewCreated(view, savedInstanceState)
+
+            val activity = requireActivity() as MainActivity
+            activity.fragment = this
+            activity.idMenuAtual = R.menu.menu_eliminar
+        }
+
+        fun processaOpcaoMenu(item: MenuItem): Boolean {
+            return when (item.itemId) {
+                R.id.action_eliminar -> {
+                    true
                 }
+                R.id.action_cancelar -> {
+                    true
+                }
+                else -> false
             }
-    }
+        }
 }
