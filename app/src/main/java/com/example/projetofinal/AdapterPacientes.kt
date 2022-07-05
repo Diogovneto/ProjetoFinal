c1edec0b6365a089ec74b68f42bf2a73a4c95a23
diogoneto <dvnetoubzgmail.com>
2022-07-05 20:53:04 +0100

    Implementada a função onBindViewHolder do AdapterPacientes e mostrar o paciente no ViewHolder

--- app/src/main/java/com/example/projetofinal/AdapterPacientes.kt
+++ app/src/main/java/com/example/projetofinal/AdapterPacientes.kt
@@ -3,6 +3,7 @@ package com.example.projetofinal
 import android.database.Cursor
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class AdapterPacientes(val fragment: ListaPacientesFragment): RecyclerView.Adapter<AdapterPacientes.ViewHolderPacientes>() {
@@ -17,6 +18,33 @@ class AdapterPacientes(val fragment: ListaPacientesFragment): RecyclerView.Adapt
         }
 
     class ViewHolderPacientes(itemPaciente: View) : RecyclerView.ViewHolder(itemPaciente) {
+        val textViewNome = itemPaciente.findViewById<TextView>(R.id.textViewNome)
+        val textViewDataNascimento = itemPaciente.findViewById<TextView>(R.id.textViewDataNascimento)
+        val textViewSexo = itemPaciente.findViewById<TextView>(R.id.textViewSexo)
+        val textViewMorada = itemPaciente.findViewById<TextView>(R.id.textViewMorada)
+        val textViewCodigoPostal = itemPaciente.findViewById<TextView>(R.id.textViewCodigoPostal)
+        val textViewTelemovel = itemPaciente.findViewById<TextView>(R.id.textViewTelemovel)
+        val textViewEmail = itemPaciente.findViewById<TextView>(R.id.textViewEmail)
+        val textViewCartaoCidadao = itemPaciente.findViewById<TextView>(R.id.textViewCartaoCidadaoPaciente)
+        val textViewContribuinte = itemPaciente.findViewById<TextView>(R.id.textViewContribuinte)
+        val textViewPulseira = itemPaciente.findViewById<TextView>(R.id.textViewPulseira)
+
+        var paciente: Paciente? = null
+            get() = field
+            set(value) {
+                field = value
+
+                textViewNome.text = paciente?.nome ?: ""
+                textViewDataNascimento.text = paciente?.data_nascimento ?: ""
+                textViewSexo.text = paciente?.sexo ?: ""
+                textViewMorada.text = paciente?.morada ?: ""
+                textViewCodigoPostal.text = paciente?.codigo_postal ?: ""
+                textViewTelemovel.text = paciente?.telemovel.toString()
+                textViewEmail.text = paciente?.email ?: ""
+                textViewCartaoCidadao.text = paciente?.cartao_cidadao.toString()
+                textViewContribuinte.text = paciente?.contribuinte.toString()
+                textViewPulseira.text = "${paciente?.id_pulseira}"
+            }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderPacientes {
@@ -26,7 +54,8 @@ class AdapterPacientes(val fragment: ListaPacientesFragment): RecyclerView.Adapt
 
 
     override fun onBindViewHolder(holder: ViewHolderPacientes, position: Int) {
-        TODO("Not yet implemented")
+        cursor!!.moveToPosition(position)
+        holder.paciente = Paciente.fromCursor(cursor!!)
     }
 
 
