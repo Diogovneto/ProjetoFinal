8986ae26fd23114533778fe7caf7bfec5d70b2ee
diogoneto <dvnetoubzgmail.com>
2022-07-10 17:26:58 +0100

    Criado Adptador de Especialidades para comunicar com o RecyclerView

---
+++ app/src/main/java/com/example/projetofinal/AdapterEspecialidades.kt
@@ -0,0 +1,23 @@
+package com.example.projetofinal
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class AdapterEspecialidades : RecyclerView.Adapter<AdapterEspecialidades.ViewHolderEspecialidades>() {
+    class ViewHolderEspecialidades(itemEspecialidade: View) : RecyclerView.ViewHolder(itemEspecialidade) {
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderEspecialidades {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolderEspecialidades, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
