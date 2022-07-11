796462cad8992d25ee753949c2706b1d0e51696e
diogoneto <dvnetoubzgmail.com>
2022-07-11 18:24:55 +0100

    Fragmento de inserir especialidade completo

--- app/src/main/java/com/example/projetofinal/InserirEspecialidadeFragment.kt
+++ app/src/main/java/com/example/projetofinal/InserirEspecialidadeFragment.kt
@@ -5,25 +5,17 @@ import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.example.projetofinal.databinding.FragmentInserirEspecialidadeBinding
+import com.google.android.material.snackbar.Snackbar
 
 
 class InserirEspecialidadeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding: FragmentInserirEspecialidadeBinding? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,34 +25,60 @@ class InserirEspecialidadeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_inserir_especialidade, container, false)
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+    
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val activity = activity as MainActivity
+        val activity = requireActivity() as MainActivity
         activity.fragment = this
         activity.idMenuAtual = R.menu.menu_edicao_especialidade
     }
 
     fun processaOpcaoMenuEspecialidade(item: MenuItem): Boolean {
         return when (item.itemId) {
-            R.id.action_guardar -> true
+            R.id.action_guardar -> {
+                guardar()
+                true
+            }
             R.id.action_cancelar -> {
-                findNavController().navigate(R.id.action_InserirEspecialidadeFragment_to_ListaEspecialidadeFragment)
+                voltaListaEspecialidades()
                 true
             }
             else -> false
         }
     }
 
-    companion object {
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            InserirEspecialidadeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun voltaListaEspecialidades() {
+        findNavController().navigate(R.id.action_InserirEspecialidadeFragment_to_ListaEspecialidadeFragment)
     }
+
+    private fun guardar() {
+        val especialidade = binding.editTextEspecialidade.text.toString()
+        if (especialidade.isBlank()) {
+            binding.editTextEspecialidade.error = getString(R.string.especialidade_obrigatoria_especialidade)
+            binding.editTextEspecialidade.requestFocus()
+            return
+        }
+
+        val especialidade_insere = Especialidade(
+            especialidade// O nome da categoria não interessa porque o que é guardado é a chave estrangeira
+        )
+
+        val endereco = requireActivity().contentResolver.insert(
+            ContentProviderConsultas.ENDERECO_ESPECIALIDADES,
+            especialidade_insere.toContentValues()
+        )
+
+        if (endereco != null) {
+            Toast.makeText(requireContext(), R.string.especialidade_inserido_sucesso, Toast.LENGTH_LONG).show()
+            voltaListaEspecialidades()
+        } else {
+            Snackbar.make(binding.editTextEspecialidade, R.string.erro_inserir_especialidade, Snackbar.LENGTH_INDEFINITE).show()
+        }
+    }
+    
 }
