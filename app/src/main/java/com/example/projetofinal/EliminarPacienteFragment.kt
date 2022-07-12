57f1159b2fa23cd6b5164f5accf35b92f4534049
diogoneto <dvnetoubzgmail.com>
2022-07-12 17:01:22 +0100

    Mudar o menu quando passamos para o fragmento eliminar paciente

--- app/src/main/java/com/example/projetofinal/EliminarPacienteFragment.kt
+++ app/src/main/java/com/example/projetofinal/EliminarPacienteFragment.kt
@@ -1,4 +1,50 @@
 package com.example.projetofinal
 
-class EliminarPacienteFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.projetofinal.databinding.FragmentEliminarPacienteBinding
+
+class EliminarPacienteFragment : Fragment() {
+    private var _binding: FragmentEliminarPacienteBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentEliminarPacienteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val activity = requireActivity() as MainActivity
+        activity.fragment = this
+        activity.idMenuAtual = R.menu.menu_eliminar_paciente
+    }
+
+    fun processaOpcaoMenu(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_eliminar -> {
+                true
+            }
+            R.id.action_cancelar -> {
+                true
+            }
+            else -> false
+        }
+    }
 }
