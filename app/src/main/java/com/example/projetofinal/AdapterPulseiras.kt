ca21bc1f7a90cd6c3cdd927a3ea01b0b9a7171a0
diogoneto <dvnetoubzgmail.com>
2022-07-12 22:55:58 +0100

    Criado Adptador de pulseiras para comunicar com o RecyclerView

--- app/src/main/java/com/example/projetofinal/AdapterPulseiras.kt
+++ app/src/main/java/com/example/projetofinal/AdapterPulseiras.kt
@@ -1,4 +1,26 @@
 package com.example.projetofinal
 
-class AdapterPulseiras {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class AdapterPulseira : RecyclerView.Adapter<AdapterPulseira.ViewHolderPulseira>() {
+    class ViewHolderPulseira(itemPulseira: View) : RecyclerView.ViewHolder(itemPulseira) {
+    }
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderPulseira {
+        TODO("Not yet implemented")
+    }
+
+
+    override fun onBindViewHolder(holder: ViewHolderPulseira, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
 }
