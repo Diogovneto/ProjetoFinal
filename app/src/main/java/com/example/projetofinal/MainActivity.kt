f9868677bf5e720197ae9840cedf191f226c704a
diogoneto <dvnetoubzgmail.com>
2022-07-12 23:30:34 +0100

    Processar as opções do menu no fragmento correto

--- app/src/main/java/com/example/projetofinal/MainActivity.kt
+++ app/src/main/java/com/example/projetofinal/MainActivity.kt
@@ -76,7 +76,11 @@ class MainActivity : AppCompatActivity() {
             opcaoProcessada = (fragment as EditarPacienteFragment).processaOpcaoMenuPaciente(item)
         } else if (fragment is EliminarPacienteFragment) {
             opcaoProcessada = (fragment as EliminarPacienteFragment).processaOpcaoMenuPaciente(item)
-        } else {
+        } else if (fragment is ListaPulseirasFragment) {
+            opcaoProcessada = (fragment as ListaPulseirasFragment).processaOpcaoMenuPulseira(item)
+        } else if (fragment is EditarPulseiraFragment) {
+            opcaoProcessada = (fragment as EditarPulseiraFragment).processaOpcaoMenuPulseira(item)
+        }else {
             opcaoProcessada = false
         }
 
