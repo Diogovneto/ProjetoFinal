b5ec8d75788a9b6d8585bfdee014609ec220e463
diogoneto <dvnetoubzgmail.com>
2022-07-15 20:54:16 +0100

    Implementado interface LoaderManager.LoaderCallbacks<Cursor>

--- app/src/main/java/com/example/projetofinal/ListaConsultasFragment.kt
+++ app/src/main/java/com/example/projetofinal/ListaConsultasFragment.kt
@@ -1,13 +1,16 @@
 package com.example.projetofinal
 
+import android.database.Cursor
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.loader.app.LoaderManager
+import androidx.loader.content.Loader
 import com.example.projetofinal.databinding.FragmentListaConsultasBinding
 
-class ListaConsultasFragment : Fragment() {
+class ListaConsultasFragment : Fragment(), LoaderManager.LoaderCallbacks<Cursor> {
 
     private var _binding: FragmentListaConsultasBinding? = null
 
@@ -34,5 +37,29 @@ class ListaConsultasFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    /**
+     * Instantiate and return a new Loader for the given ID.
+     *
+     *
+     * This will always be called from the process's main thread.
+     *
+     * @param id The ID whose loader is to be created.
+     * @param args Any arguments supplied by the caller.
+     * @return Return a new Loader instance that is ready to start loading.
+     */
+    override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> {
+        TODO("Not yet implemented")
+    }
+
+
+    override fun onLoadFinished(loader: Loader<Cursor>, data: Cursor?) {
+        TODO("Not yet implemented")
+    }
+
+
+    override fun onLoaderReset(loader: Loader<Cursor>) {
+        TODO("Not yet implemented")
     }
 }
