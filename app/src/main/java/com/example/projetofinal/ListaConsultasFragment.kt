69677e827a44e729d3113aae768217932170e1ac
diogoneto <dvnetoubzgmail.com>
2022-07-15 20:51:31 +0100

    Desenhado interface do fragmento para mostrar a lista de consultas

--- app/src/main/java/com/example/projetofinal/ListaConsultasFragment.kt
+++ app/src/main/java/com/example/projetofinal/ListaConsultasFragment.kt
@@ -1,4 +1,38 @@
 package com.example.projetofinal
 
-class ListaConsultasFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.projetofinal.databinding.FragmentListaConsultasBinding
+
+class ListaConsultasFragment : Fragment() {
+
+    private var _binding: FragmentListaConsultasBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentListaConsultasBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //binding.buttonSecond.setOnClickListener {
+        //    findNavController().navigate(R.id.action_ListaConsultasFragment_to_MenuPrincipalFragment)
+        //}
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
