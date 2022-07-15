8f43bf86cdbca392fbcb908bd80f6a6b97e57841
diogoneto <dvnetoubzgmail.com>
2022-07-15 21:18:56 +0100

    Criado Adptador de consultas para comunicar com o RecyclerView

--- app/src/main/java/com/example/projetofinal/AdapterConsultas.kt
+++ app/src/main/java/com/example/projetofinal/AdapterConsultas.kt
@@ -1,4 +1,25 @@
 package com.example.projetofinal
 
-class AdapterConsultas {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class AdapterConsultas : RecyclerView.Adapter<AdapterConsultas.ViewHolderConsultas>() {
+    class ViewHolderConsultas(itemConsulta: View) : RecyclerView.ViewHolder(itemConsulta) {
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderConsultas {
+        TODO("Not yet implemented")
+    }
+
+
+    override fun onBindViewHolder(holder: ViewHolderConsultas, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
 }
